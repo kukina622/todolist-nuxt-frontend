022923509bc95a7fec573bdfff9601fcdc156cda
min <b10923012@yuntech.edu.tw>
2021-10-20 17:21:12 +0800

    Add logout button in navigator

--- components/navigator.vue
+++ components/navigator.vue
@@ -4,14 +4,21 @@
       <h1 class="text-h6">To Do List</h1>
     </v-app-bar-title>
     <v-spacer></v-spacer>
-    <v-btn color="#FFFFFF" nuxt outlined :to="{ name: 'register' }">註冊</v-btn>
-    <v-btn
-      nuxt
-      color="#FFFFFF"
-      class="ml-3 light-blue--text text--accent-4"
-      :to="{ name: 'login' }"
-      >登入
-    </v-btn>
+    <template v-if="!isLogin">
+      <v-btn color="#FFFFFF" nuxt outlined :to="{ name: 'register' }">
+        註冊
+      </v-btn>
+      <v-btn
+        nuxt
+        color="#FFFFFF"
+        class="ml-3 light-blue--text text--accent-4"
+        :to="{ name: 'login' }"
+        >登入
+      </v-btn>
+    </template>
+    <template v-else>
+      <v-btn color="#FFFFFF" outlined @click="logout">登出</v-btn>
+    </template>
   </v-app-bar>
 </template>
 
@@ -21,6 +28,13 @@ export default {
     isLogin: {
       type: Boolean,
       default: false,
+    },
+  },
+  methods: {
+    async logout() {
+      await this.$api.user.logout()
+      this.$alert.success('登出成功')
+      this.$router.app.refresh()
     },
   },
 }
